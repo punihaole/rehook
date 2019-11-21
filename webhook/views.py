6bb946efc43a10981a8e554eb53ce3db2afda40e
Tom Punihaole <tom.punihaole@gmail.com>
2019-11-20 20:46:50 -0800

    Add pagination to webhook views

--- webhook/views.py
+++ webhook/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 
 from webhook.serializers import WebhookSerializer
 from .models import Webhook
@@ -40,7 +41,14 @@ class GenericWebhookHandler(APIView):
         return self.handler(request, *args, **kwargs)
 
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class WebhookViewSet(viewsets.ModelViewSet):
     queryset = Webhook.objects.all()
     serializer_class = WebhookSerializer
     lookup_field = 'rehook_id'
+    pagination_class = StandardResultsSetPagination
